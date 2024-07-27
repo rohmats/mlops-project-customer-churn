9d4a99a7e96bdf56d431063f894325270f3d569a
Rohmat S <61409906+rohmats@users.noreply.github.com>
2024-07-27 18:03:34 +0000

    integrate with dagshub mlflow

--- src/data/data.py
+++
@@ -1,9 +0,0 @@
-import dagshub
-dagshub.init(repo_owner='rohmats',
-             repo_name='mlops-project-customer-churn',
-             mlflow=True)
-
-import mlflow
-with mlflow.start_run():
-  mlflow.log_param('parameter name', 'value')
-  mlflow.log_metric('metric name', 1)
